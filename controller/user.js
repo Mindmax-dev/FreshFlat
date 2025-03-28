02026edbe0aa6c022a8c729fda27e3a6f0dac6a2
ramsh721 <ramsh721@student.otago.ac.nz>
2025-03-28 20:49:17 +1300

    D4 sign in user function mvc pattern 2nd try

--- controller/user.js
+++ controller/user.js
@@ -1,3 +1,8 @@
-export default async function signUpUser(email, password, username) {
+import signUpUser, { signInUser } from "@/model/user";
+
+export async function signUpUserController(email, password, username) {
   return await signUpUser(email, password, username);
+}
+export async function signInUserController(email, password) {
+  return await signInUser(email, password);
 }
--- model/user.js
+++ model/user.js
@@ -15,17 +15,12 @@ export default async function signUpUser(email, password, username) {
 
   return { data, error };
 }
-export async function loginUserUser(email, password, username) {
+export async function signInUser(email, password) {
   const supabase = createClient();
 
-  const { data, error } = await supabase.auth.signUp({
+  const { user, session, error } = await supabase.auth.signInWithPassword({
     email,
     password,
-    options: {
-      data: {
-        full_name: username,
-      },
-    },
   });
-  return { data, error };
+  return { user, session, error };
 }
