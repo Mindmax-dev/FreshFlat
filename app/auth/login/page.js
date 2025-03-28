d44cb1587d8c32798f450b3ab9fdcd17d1990735
ramsh721 <ramsh721@student.otago.ac.nz>
2025-03-28 20:54:50 +1300

    D4 sign in user function mvc pattern 3rd try

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,8 +1,9 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
-import { createClient } from '@/utils/supabase/client';
-import { useEffect } from 'react';
+"use client";
+import { signInUserController } from "@/controller/user";
+import { createClient } from "@/utils/supabase/client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Login() {
   const router = useRouter();
@@ -18,18 +19,17 @@ export default function Login() {
 
   const loginUser = async (event) => {
     event.preventDefault();
-    const supabase = createClient();
     const email = event.target.elements[0].value;
     const password = event.target.elements[1].value;
-    const { user, session, error } = await supabase.auth.signInWithPassword({
+    const { user, session, errro } = await signInUserController(
       email,
-      password,
-    });
+      password
+    );
     console.log(user, session, error);
     if (error) {
       alert(error.message);
     } else {
-      router.push('/');
+      router.push("/");
     }
   };
 
@@ -52,7 +52,7 @@ export default function Login() {
         Version 1: Do not have an account yet? Sign Up!
       </Link>
       <br></br>
-      <button onClick={() => router.push('/auth/signup')}>
+      <button onClick={() => router.push("/auth/signup")}>
         Version 2: Do not have an account yet? Sign Up!
       </button>
     </div>
