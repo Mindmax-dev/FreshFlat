c9ee5f43807dcd511df0e65b5519e5686c787130
ramsh721 <ramsh721@student.otago.ac.nz>
2025-04-06 19:12:28 +1200

    Created a simple page for creating and joining a flat, functionalities are left to be added.

---
+++ app/chooseFlat/page.js
@@ -0,0 +1,14 @@
+export default function chooseFlat() {
+  return (
+    <div className="container">
+      <div className="box">
+        <div>CREATE FLAT</div>
+        <input type="text" placeholder="Enter flat code" />
+      </div>
+      <div className="box">
+        <div>JOIN FLAT</div>
+        <input type="text" placeholder="Enter flat code" />
+      </div>
+    </div>
+  );
+}
