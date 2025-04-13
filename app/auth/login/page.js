8ec7f2778fd6bb61324fa0bca3785ad3a5002110
Max Beutler <mindmax2016@gmail.com>
2025-04-13 16:09:21 +1200

    simple ui styling for sign up and login to make it more appealing for demo

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,20 +1,39 @@
-'use server';
-
 import { handleLogin } from './actions';
+import styles from './page.module.css';
 
 export default async function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
-      <p>Log in to your account</p>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
-        <button formAction={handleLogin}>Log in</button>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Login</h1>
+      <p className={styles.subheading}>Log in to your account</p>
+      <form className={styles.form}>
+        <label htmlFor="email" className={styles.label}>
+          Email:
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          required
+          className={styles.input}
+        />
+
+        <label htmlFor="password" className={styles.label}>
+          Password:
+        </label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          required
+          className={styles.input}
+        />
+
+        <button formAction={handleLogin} className={styles.button}>
+          Log in
+        </button>
       </form>
-      <p>
+      <p className={styles.linkText}>
         Don&apos;t have an account? <a href="/auth/signup">Sign up here</a>.
       </p>
     </div>
--- app/auth/signup/page.js
+++ app/auth/signup/page.js
@@ -1,25 +1,42 @@
 import Link from 'next/link';
 import { handleSignUp } from './actions';
+import styles from './page.module.css';
 
 export default function SignUp() {
   return (
-    <div>
-      <h1>Sign Up!</h1>
-      <p>Create your account</p>
-      <form>
-        <input type="text" name="username" placeholder="Username" required />
-        <input type="email" name="email" placeholder="Email" required />
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Sign Up!</h1>
+      <p className={styles.subheading}>Create your account</p>
+      <form className={styles.form}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          required
+          className={styles.input}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
         <input
           type="password"
           name="password"
           placeholder="Password"
           required
+          className={styles.input}
         />
-        <button formAction={handleSignUp}>Sign Up</button>
+        <button formAction={handleSignUp} className={styles.button}>
+          Sign Up
+        </button>
       </form>
 
-      <br />
-      <Link href="/auth/login">Already have an account? Log In!</Link>
+      <p className={styles.linkText}>
+        <Link href="/auth/login">Already have an account? Log In!</Link>
+      </p>
     </div>
   );
 }
