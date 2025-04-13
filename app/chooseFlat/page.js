35d263b2df3a6c1bc139c25e8c3e400583fc6b55
ramsh721 <ramsh721@student.otago.ac.nz>
2025-04-13 20:06:15 +1200

    minor change to layout

--- app/chooseFlat/page.js
+++ app/chooseFlat/page.js
@@ -1,25 +1,34 @@
-export default function chooseFlat() { 
-
+export default function chooseFlat() {
   return (
     <div
       className="container"
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        flexDirection: "column",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
       }}
     >
-      <div className="box" style={{ marginBottom: "20px", textAlign: "center" }}>
-        <div>CREATE FLAT</div>
-        <input type="text" placeholder="Enter flat name" />
-        <button>Done</button>
-      </div>
-      <div className="box" style={{ textAlign: "center" }}>
-        <div>JOIN FLAT</div>
-        <input type="text" placeholder="Enter flat code" />
-        <button>Done</button>
+      <div
+        className="box"
+        style={{
+          border: '1px solid black',
+          padding: '20px',
+          borderRadius: '10px',
+          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+          textAlign: 'center',
+        }}
+      >
+        <div style={{ marginBottom: '20px' }}>
+          <div>CREATE FLAT</div>
+          <input type="text" placeholder="Enter flat name" />
+          <button>Done</button>
+        </div>
+        <div>
+          <div>JOIN FLAT</div>
+          <input type="text" placeholder="Enter flat code" />
+          <button>Done</button>
+        </div>
       </div>
     </div>
   );
