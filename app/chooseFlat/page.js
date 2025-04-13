e4d99d528cb52ae4bd1299a2f3ebf441b5da2db7
ramsh721 <ramsh721@student.otago.ac.nz>
2025-04-13 20:03:51 +1200

    Worked on the layout of the page, positioned it in the center

--- app/chooseFlat/page.js
+++ app/chooseFlat/page.js
@@ -1,12 +1,22 @@
-export default function chooseFlat() {
+export default function chooseFlat() { 
+
   return (
-    <div className="container">
-      <div className="box">
+    <div
+      className="container"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <div className="box" style={{ marginBottom: "20px", textAlign: "center" }}>
         <div>CREATE FLAT</div>
         <input type="text" placeholder="Enter flat name" />
         <button>Done</button>
       </div>
-      <div className="box">
+      <div className="box" style={{ textAlign: "center" }}>
         <div>JOIN FLAT</div>
         <input type="text" placeholder="Enter flat code" />
         <button>Done</button>
