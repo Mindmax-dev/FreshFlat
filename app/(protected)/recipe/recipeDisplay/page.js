90ecb22ed7ced44ebedd0cc2b3523448cc996d42
Matthew Rae <mattyjrae123@hotmail.com>
2025-05-17 14:12:15 +1200

    Add base styling. Half finished

--- app/(protected)/recipe/recipeDisplay/page.js
+++ app/(protected)/recipe/recipeDisplay/page.js
@@ -1,15 +1,35 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
+import styles from './page.module.css';
 
-export default function SelectIngredients() {
+export default function RecipeDisplay() {
   const searchParams = useSearchParams();
   return (
-    <>
-      <p>{searchParams.get('title')}</p>
-      <p>{searchParams.get('instructions')}</p>
-      <p>{searchParams.get('difficulty')}</p>
-      <p>{searchParams.get('preparation_time')}</p>
-      <p>{searchParams.get('cooking_time')}</p>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.recipeTitle}>{searchParams.get('title')}</h1>
+      <span className={styles.subTitleContainer}>
+        <p className={styles.preparationTime}>
+          Preparation time: {searchParams.get('preparation_time')} mins
+        </p>
+        <p className={styles.cookingTime}>
+          Cooking time: {searchParams.get('cooking_time')} mins
+        </p>
+      </span>
+      <p className={styles.difficulty}>
+        Difficulty: {searchParams.get('difficulty')}
+      </p>
+      <ol className={styles.instructions}>
+        <h2 className={styles.instructionTitle}>Instructions</h2>
+        {searchParams
+          .get('instructions')
+          .split(/\d+\./) // split the instructions on each (number + '.')
+          .filter(Boolean)
+          .map((instruction, index) => (
+            <li className={styles.instruction} key={index}>
+              {instruction}
+            </li>
+          ))}
+      </ol>
+    </div>
   );
 }
